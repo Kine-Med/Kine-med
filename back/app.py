2966d7e0f78565037e65839051371c5978ed6c94
Mathias Filliol <mathias.filliol@epita.fr>
2023-11-12 00:16:32 +0100

    feat(back): add back camera

--- back/app.py
+++ back/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, request
-import numpy as np 
+from flask import Flask, request, render_template, Response
+import numpy as np
 import base64
 from flask_cors import CORS
 import cv2, io
+from pathlib import Path
+from camera import Camera
 
 app = Flask(__name__)
 CORS(app)
+
+camera = Camera()
+camera.run()
+
+@app.route("/video_feed")
+def video_feed():
+	return Response(gen(camera),
+		mimetype="multipart/x-mixed-replace; boundary=frame")
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+			   b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
 
 def decode_base64_image(data):
     base64_image = data
