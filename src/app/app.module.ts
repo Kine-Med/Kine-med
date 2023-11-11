3c8c728e8567f0283c71996245ee79f32dd53028
Tristan GADAUD <tristan.gadaud@epita.fr>
2023-11-11 02:43:36 +0100

    add files struct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoComponent } from './video/video.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
